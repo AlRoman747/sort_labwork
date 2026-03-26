62a4e2f7f05c8a20a57adef48eb83583d269c491
AlRoman747 <romaaltahir@icloud.com>
2026-03-26 11:13:45 +0300

    add gitignore and keys-array

--- sorting.c
+++ sorting.c
@@ -6,13 +6,18 @@ int main(void)
 {
     FILE *fp = fopen("sorting_data.txt", "r");
 
+    int v = 15;
     char buffer[256];
     char *line = NULL;
-    size_t line_len = 0;
+    int line_len = 0;
 
-    while (fgets(buffer, sizeof(buffer), fp))
-    {
-        size_t chunk_len = strlen(buffer);
+    int *arr = malloc(sizeof(int) * v);
+    if (!arr) return 1;
+    int len_arr = 0;
+
+
+    while (fgets(buffer, sizeof(buffer), fp)) {
+        int chunk_len = strlen(buffer);
         char *tmp = realloc(line, line_len + chunk_len + 1);
         if (!tmp) {
             free(line);
@@ -22,10 +27,19 @@ int main(void)
 
         // add part of line
         memcpy(line + line_len, buffer, chunk_len);
-        line_len += chunk_len;
+        line_len += chunk_len; // increase line len
         line[line_len] = '\0';
 
-        // if all okey
+        arr[len_arr++] = buffer[0];
+
+        if (len_arr == v) {
+            v *= 2;
+            arr = realloc(arr, sizeof(int) * v);
+            if (!arr) return 1;
+        }
+
+        // if len line in buffer size
+        /*
         if (buffer[chunk_len - 1] == '\n')
         {
             printf("%s", line);
@@ -33,8 +47,17 @@ int main(void)
             line = NULL;
             line_len = 0;
         }
+
+    printf("\n");
+    */
     }
+    for (int i=0; i < len_arr; i++) {
+        printf("%c ", arr[i]);
+    }
+
+
 
     free(line);
     fclose(fp);
+    return 0;
 }
